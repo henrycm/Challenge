b84fcc265d46e375959011cfc2dbc2a3280e29f0
johecama <henrycm@gmail.com>
2015-06-27 17:31:50 -0400

    Fix bug with placeholder

--- src/main/java/com/jhcm/appdirect/config/MVCConfig.java
+++ src/main/java/com/jhcm/appdirect/config/MVCConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -38,8 +37,4 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
 		return resolver;
 	}
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
 }
