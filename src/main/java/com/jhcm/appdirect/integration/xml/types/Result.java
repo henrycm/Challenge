258d7fe349990485bf16773c852481f652d5c988
johecama <henrycm@gmail.com>
2015-06-27 22:11:13 -0400

    Add xml test message as result

---
+++ src/main/java/com/jhcm/appdirect/integration/xml/types/Result.java
@@ -0,0 +1,91 @@
+package com.jhcm.appdirect.integration.xml.types;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.jhcm.appdirect.integration.xml.ErrorCode;
+
+@XmlRootElement(name = "result")
+public class Result implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private boolean success;
+	private boolean asynchronous = false;
+	private ErrorCode errorCode;
+	private String message;
+	private String accountIdentifier;
+	private String userIdentifier;
+	private String id;
+
+	public Result() {
+	}
+
+	public Result(boolean success, String message) {
+		this(success, null, message);
+	}
+
+	public Result(boolean success, ErrorCode errorCode, String message) {
+		super();
+		this.success = success;
+		this.errorCode = errorCode;
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public boolean isAsynchronous() {
+		return asynchronous;
+	}
+
+	public void setAsynchronous(boolean asynchronous) {
+		this.asynchronous = asynchronous;
+	}
+
+	public ErrorCode getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(ErrorCode errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getAccountIdentifier() {
+		return accountIdentifier;
+	}
+
+	public void setAccountIdentifier(String accountIdentifier) {
+		this.accountIdentifier = accountIdentifier;
+	}
+
+	public String getUserIdentifier() {
+		return userIdentifier;
+	}
+
+	public void setUserIdentifier(String userIdentifier) {
+		this.userIdentifier = userIdentifier;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
--- src/main/java/com/jhcm/appdirect/view/AppDirectEventController.java
+++ src/main/java/com/jhcm/appdirect/view/AppDirectEventController.java
@@ -1,25 +1,26 @@
 package com.jhcm.appdirect.view;
 
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.jhcm.appdirect.integration.xml.types.Result;
+
+@RestController
 @RequestMapping("/rest/event")
 public class AppDirectEventController {
 
 	private Logger log = Logger.getLogger(AppDirectEventController.class);
 
 	@RequestMapping(method = RequestMethod.GET)
-	@ResponseBody
-	public String event(@RequestParam(value = "eventUrl", required = false) String eventUrl,
+	public Result event(@RequestParam(value = "eventUrl", required = false) String eventUrl,
 			@RequestParam(value = "token", required = false) String token) {
 		log.debug("Event!");
 		log.debug("Url:" + eventUrl);
 		log.debug("token:" + token);
-		return "OK";
+		
+		return new Result(true, "Test message");
 	}
 }
